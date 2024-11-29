53293513f92f23f486e028a4377c5f7e5fb85b38
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2024-11-29 16:00:25 +0100

    Fix flowbite javascript

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -4,4 +4,6 @@ import "$styles/syntax-highlighting.css"
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
 
+import 'flowbite';
+
 console.info("Bridgetown is loaded!")
